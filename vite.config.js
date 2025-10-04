0f57685b062fc3b500219833ab31f306f14715c9
divyeshdv7 <divyeshvasavadv10@gmail.com>
2025-10-04 17:33:30 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://divyeshdv7.github.io/t2", // <-- your GitHub repo name
   plugins: [react()],
 })
